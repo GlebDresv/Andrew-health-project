7986fbb7682dd007b218c0c44c5c8b9117e68e3d
hydra <a.goloshevich@gmail.com>
2022-04-14 22:43:32 +0300

    First attempt

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,32 @@
 <?php
+$user = "root";
+$pass = "root";
+$peoples = [["Пупкин Степан Леонидович","Киев","120"],["Акопян Светлана Юриевна","Ивано-Франковск","74"],["Заболотная Юлия Сергеевна","Лондон","87"]];
+try {
+    $dbh = new PDO('mysql:host=mysql;dbname=db', $user, $pass);
+    echo "Подключились\n";
+} catch (Exception $e) {
+    die("Не удалось подключиться: " . $e->getMessage());
+}
 
-echo 'Hello world';
+try {
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->beginTransaction();
+    foreach ($peoples as $people) {
+        $dbh->exec("insert into db (name, city, mass) values ($people[1], $people[2], $people[3])");
+    }
+    $dbh->commit();
+    $dbh = null;
+}
+catch (Exception $e) {
+    $dbh->rollBack();
+    echo "Ошибка: " . $e->getMessage();
+}
+
+?>
+
+
+
+
+
+
