5ab5b7f1052cdb42c3f4a08c441bd84f88f93833
hydra <a.goloshevich@gmail.com>
2022-05-11 19:05:30 +0300

    2022.04.11
    Created profile.php and change index.php

--- app/frontend/views/site/profile.php
+++ app/frontend/views/site/profile.php
@@ -1,6 +1,10 @@
 <?php
 
-/** @var yii\web\View $this */
+/**
+ * @var frontend\controllers\SiteController $personal_info
+ * @var frontend\controllers\SiteController $public_info
+ * @var yii\web\View $this
+ */
 
 use yii\helpers\Html;
 
@@ -8,7 +12,16 @@ $this->title = 'Profile';
 ?>
 <div class="site-profile">
     <h1><?= Html::encode($this->title) ?></h1>
-    <?= var_dump($user) ?>
-    <?php exit;?>
-    <img src="<?= $user_auth->image ?>">
+    <img src="<?= $public_info->image ?>" width="220px" height="220px" border-radius="50%" alt="">
+    <p><b><?= $personal_info->username . ' (' . $public_info->full_name . ')' ?></b>
+        <br><?= $personal_info->email ?><br>
+        <?= $public_info->phone_number ?></p>
+    <p><?= $public_info->about ?></p>
+    Distance:
+    <ul>
+        <li>Today: <?= $public_info->distance_day ?></li>
+        <li>Week: <?= $public_info->distance_week ?></li>
+        <li>All: <?= $public_info->distance_all ?></li>
+    </ul>
+    <p>desired: <?= $public_info->desired_distance ?> | recommended: <?= $public_info->recommended_distance ?></p>
 </div>
