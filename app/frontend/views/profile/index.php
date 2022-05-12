3466da86e9b62ffb88c0d9c5e7418d7411f5c51b
hydra <a.goloshevich@gmail.com>
2022-05-12 21:23:59 +0300

    2022.04.12
    better method

--- app/frontend/views/profile/index.php
+++ app/frontend/views/profile/index.php
@@ -2,7 +2,6 @@
 
 use common\models\ProfileInfo;
 use common\models\User;
-use frontend\models\ProfileForm;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -11,7 +10,6 @@ use yii\widgets\ActiveForm;
  * @var yii\web\View $this
  * @var User $userInfo
  * @var ProfileInfo $profileInfo
- * @var ProfileForm $model
  */
 
 
@@ -20,24 +18,15 @@ $this->title = 'Profile';
 <div class="profile-index">
     <h1><?= Html::encode($this->title) ?></h1>
     <img src="<?= $profileInfo->image ?>" width="220px" height="200px" border-radius="50%" alt="">
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'fullName')->textInput(['value' => $profileInfo->full_name]) ?>
-
-    <?= $form->field($model, 'phoneNumber')->textInput(['type' => 'number', 'value' => $profileInfo->phone_number]) ?>
-
-    <?= $form->field($model, 'about')->textInput([ 'value' => $profileInfo->about]) ?>
-
-    <?= $form->field($model, 'age')->textInput(['type' => 'number', 'value' => $profileInfo->age]) ?>
-
-    <?= $form->field($model, 'height')->textInput(['type' => 'number', 'value' => $profileInfo->height]) ?>
-
-    <?= $form->field($model, 'desiredDistance')->textInput(['type' => 'number', 'value' => $profileInfo->desired_distance]) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
+    <p><b><?= $userInfo->username . ' (' . $profileInfo->full_name . ')' ?></b>
+        <br><?= $userInfo->email ?><br>
+        <?= $profileInfo->phone_number ?></p>
+    <p><?= $profileInfo->about ?></p>
+    Distance:
+    <ul>
+        <li>Today: <?= $profileInfo->distance_day ?></li>
+        <li>Week: <?= $profileInfo->distance_week ?></li>
+        <li>All: <?= $profileInfo->distance_all ?></li>
+    </ul>
+    <p>desired: <?= $profileInfo->desired_distance ?> | recommended: <?= $profileInfo->recommended_distance ?></p>
 </div>
